7c0e53827f8c5bf20fdcc37b80c1b0a3c579668c
Alex Worrad-Andrews <alex@alexandrews.info>
2024-03-06 12:47:37 +0000

    Remove debugging code

--- utils/cached_fn.py
+++ utils/cached_fn.py
@@ -56,7 +56,6 @@ def cache_resolve_many(keys, resolve, bucket, get_key=id_key, cache_type='defaul
     }
 
 def cache_resolve(key, resolve, bucket, cache_type='default', timeout_seconds=None):
-    # print('lookup', [bucket, key])
     
     cache = caches[cache_type]
 
